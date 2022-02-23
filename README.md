d2caa278636965cbf8b3a43babfedbc3a7f2b3f9
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2022-02-23 08:51:45 +0400

    upd

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Webpack - Полный курс за 3 часа [2020]
-  https://youtu.be/eSaF8NXeNsA?t=3872
+  https://youtu.be/eSaF8NXeNsA?t=6150
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,43 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: "development",
   entry: {
-    header: './src/js/header/scripts.js',
-    footer: './src/js/footer/scripts.js',
+    scripts: './src/js/scripts.js',
   },
   output: {
     filename: '[name].bundle.js',//[contenthash], [hash]
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    extensions: ['js', 'json'],//чтобы расширения не писать при импорте
+    alias: {
+      '@models': path.resolve(__dirname, './src/models/'),//чтобы импортировать относительные пути: @models/Model.js
+      '@': path.resolve(__dirname, './src/'),
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  devServer: {
+    port: 2000
+  },
   plugins: [
-    // new HTMLWebpackPlugin({
-    //   template: './src/index.html',
-    // }),
-    // new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
   ],
   module: {
     rules: [
@@ -27,6 +48,18 @@ module.exports = {
       {
         test: /\.(png|jpg|svg|gif)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      },
+      {
+        test: /\.csv$/,
+        use: ['csv-loader']
       }
     ]
   }
